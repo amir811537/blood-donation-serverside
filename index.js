7a473db6997d5dc8a89a78e08cf2fc37101dc286
amir811537 <amirhossain.bc.75@gmail.com>
2023-11-29 22:13:42 +0600

    added admin api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -10,9 +10,6 @@ const port = process.env.PORT || 5000;
 // middleware 
 app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@hotelhaven-database.n0h5vlk.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -38,33 +35,162 @@ async function run() {
   }
 }
 
+const userCollection = client.db('BloodDonation').collection('users');
+const donationCollection = client.db('BloodDonation').collection('donation');
+// blood donation related api
+app.post('/donation', async (req, res) => {
+  const donor = req.body;
+  const result = await donationCollection.insertOne(donor);
+  res.send(result)
+})
 
-const userCollection=client.db('BloodDonation').collection('users');
+// get the donation request
+app.get('/donation', async (req, res) => {
+  const result = await donationCollection.find().toArray();
+  res.send(result)
+})
+// delete the donation req
+app.delete('/donation/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+  const result = await donationCollection.deleteOne(query);
+  res.send(result)
+})
+// get donation by id
+app.get('/donation/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await donationCollection.findOne(query);
+  res.send(result)
+})
 
 
+
+// PATCH endpoint to update a donation request
+app.patch('/donation/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+
+    // Assuming req.body contains the fields you want to update
+    const updateFields = { $set: req.body };
+
+    const result = await donationCollection.updateOne(query, updateFields);
+
+    if (result.modifiedCount > 0) {
+      res.status(200).send({ message: 'Donation request updated successfully.' });
+    } else {
+      res.status(404).send({ error: 'Donation request not found.' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Internal server error.' });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// User related api
 // user added to mongobd
 app.post("/users", async (req, res) => {
-    const user = req.body;
-    // insert email if user doesnt exists
-  
-    // you can do this many ways(1.email unique ,2.upsert,3.simple checking )
-  
-    const query = { email: user.email };
-    const existingUser = await userCollection.findOne(query);
-    if (existingUser) {
-      return res.send({ message: "user already exists", insertedId: null });
-    }
-  
-    const result = await userCollection.insertOne(user);
-    res.send(result);
-  });
+  const user = req.body;
+  // insert email if user doesnt exists
 
-  // get the user
-app.get("/users",  async (req, res) => {
-    // console.log(req.headers);
-    const result = await userCollection.find().toArray();
-    res.send(result);
-  });
+  // you can do this many ways(1.email unique ,2.upsert,3.simple checking )
+
+  const query = { email: user.email };
+  const existingUser = await userCollection.findOne(query);
+  if (existingUser) {
+    return res.send({ message: "user already exists", insertedId: null });
+  }
+
+  const result = await userCollection.insertOne(user);
+  res.send(result);
+});
+
+// user delete api
+app.delete("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await userCollection.deleteOne(query);
+  res.send(result)
+})
+
+// make adminand volunteer  api
+app.patch('/users/admin/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const options = { upsert: true };
+  const updatedDoc = {
+    $set: {
+      role: "admin"
+    }
+  }
+  const result = await userCollection.updateOne(filter, updatedDoc, options);
+  res.send(result)
+
+})
+// Make user a volunteer API
+app.patch('/users/volunteer/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const options = { upsert: true };
+  const updatedDoc = {
+    $set: {
+      role: "volunteer"
+    }
+  }
+  const result = await userCollection.updateOne(filter, updatedDoc, options);
+  res.send(result);
+});
+
+
+
+// get the user
+app.get("/users", async (req, res) => {
+  // console.log(req.headers);
+  const result = await userCollection.find().toArray();
+  res.send(result);
+});
+
+
+app.patch('/users/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+
+  try {
+    // Assuming req.body contains the fields you want to update
+    const updatedData = req.body;
+
+    // Update the user information in the database
+    const result = await userCollection.updateOne(query, { $set: updatedData });
+
+    if (result.modifiedCount > 0) {
+      res.send({ message: 'User updated successfully' });
+    } else {
+      res.status(404).send({ message: 'User not found or no updates applied' });
+    }
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).send({ message: 'Internal server error' });
+  }
+});
+
+app.get('/users/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await userCollection.findOne(query);
+  res.send(result)
+})
 
 
 
@@ -73,11 +199,11 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send(' blood  donation is running.......')
+  res.send(' blood  donation is running.......')
 });
 
 
 app.listen(port, () => {
-    console.log(`blood bonation server is running on the port ${port}`)
+  console.log(`blood bonation server is running on the port ${port}`)
 }
 )
